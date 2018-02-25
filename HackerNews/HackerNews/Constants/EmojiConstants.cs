62b7221fcf3a3d081f294ac6bca8407e534713fb
Brandon Minnick <brandon.r.minnick@gmail.com>
2018-02-25 10:07:56 -0800

    Updated Emoji Logic

--- HackerNews/HackerNews/Constants/EmojiConstants.cs
+++ HackerNews/HackerNews/Constants/EmojiConstants.cs
@@ -5,5 +5,6 @@
         public const string SadFaceEmoji = "☹️";
         public const string NeutralFaceEmoji = "\U0001F610";
         public const string HappyFaceEmoji = "\U0001F603";
+        public const string BlankFaceEmoji = "\U0001F636";
     }
 }
--- HackerNews/HackerNews/Models/StoryModel.cs
+++ HackerNews/HackerNews/Models/StoryModel.cs
@@ -8,8 +8,9 @@ namespace HackerNews
     public class StoryModel
     {
         public DateTimeOffset CreatedAt_DateTimeOffset => UnixTimeStampToDateTimeOffset(CreatedAt_UnixTime);
+        public string TitleSentimentEmoji => GetEmoji(TitleSentimentScore);
 
-        public float? TitleSentimentScore { get; set; }
+        public float? TitleSentimentScore { get; set; } = -1;
 
         [JsonProperty("by")]
         public string Author { get; set; }
@@ -42,6 +43,23 @@ namespace HackerNews
         {
             var dateTimeOffset = new DateTimeOffset(1970, 1, 1, 0, 0, 0, 0, default);
             return dateTimeOffset.AddSeconds(unixTimeStamp);
+        }
+
+        string GetEmoji(float? sentimentScore)
+        {
+            switch (sentimentScore)
+            {
+                case float number when (number >= 0 && number < 0.4):
+                    return EmojiConstants.SadFaceEmoji;
+                case float number when (number >= 0.4 && number <= 0.6):
+                    return EmojiConstants.NeutralFaceEmoji;
+                case float number when (number > 0.6):
+                    return EmojiConstants.HappyFaceEmoji;
+                case null:
+                    return EmojiConstants.BlankFaceEmoji;
+                default:
+                    return string.Empty;
+            }
         }
     }
 }
