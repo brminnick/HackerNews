86c961acb87daad35394100c4a32fcafbdaaacb0
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2018-09-06 11:20:02 -0700

    Update TextAnalysisConstants.cs

--- HackerNews/HackerNews/Constants/TextAnalysisConstants.cs
+++ HackerNews/HackerNews/Constants/TextAnalysisConstants.cs
@@ -7,7 +7,7 @@
         public const string SentimentKey = "Your Sentiment API Key";
 
 #Error Missing Text Analytics Base Url
-        //FaceApiBaseUrl example: https://westus.api.cognitive.microsoft.com/
+        //BaseUrl example: https://westus.api.cognitive.microsoft.com/
         public const string BaseUrl = "Your Sentiment API Base Url";
     }
 }
