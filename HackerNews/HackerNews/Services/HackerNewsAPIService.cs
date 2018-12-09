26cd71fd48835ccbd2b746b84a047fc4a6f507d2
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2018-12-09 08:45:45 +0100

    Added Polly & Refit

--- HackerNews/HackerNews/Services/HackerNewsAPIService.cs
+++ HackerNews/HackerNews/Services/HackerNewsAPIService.cs
@@ -1,14 +1,42 @@
-﻿using System.Threading.Tasks;
+﻿using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Polly;
+using Refit;
 
 namespace HackerNews
 {
-    public abstract class HackerNewsAPIService : BaseHttpClientService
+    public static class HackerNewsAPIService
     {
-        public static Task<List<string>> GetTopStoryIDs() =>
-            GetObjectFromAPI<List<string>>("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
+        #region Constant Fields
+        readonly static Lazy<IHackerNewsAPI> _hackerNewsApiClientHolder = new Lazy<IHackerNewsAPI>(() => RestService.For<IHackerNewsAPI>("https://hacker-news.firebaseio.com/v0"));
 
-        public static Task<StoryModel> GetStory(string storyId) =>
-		    GetObjectFromAPI<StoryModel>($"https://hacker-news.firebaseio.com/v0/item/{storyId}.json?print=pretty");
+        #endregion
+
+        #region Properties
+        static IHackerNewsAPI HackerNewsApiClient => _hackerNewsApiClientHolder.Value;
+        #endregion
+
+        #region Methods
+        public static Task<StoryModel> GetStory(string storyId) => ExecutePollyHttpFunction(() => HackerNewsApiClient.GetStory(storyId));
+        public static Task<List<string>> GetTopStoryIDs() => ExecutePollyHttpFunction(() => HackerNewsApiClient.GetTopStoryIDs());
+
+        static Task<T> ExecutePollyHttpFunction<T>(Func<Task<T>> action, int numRetries = 5)
+        {
+            return Policy
+                    .Handle<WebException>()
+                    .Or<HttpRequestException>()
+                    .Or<TimeoutException>()
+                    .WaitAndRetryAsync
+                    (
+                        numRetries,
+                        pollyRetryAttempt
+                    ).ExecuteAsync(action);
+
+            TimeSpan pollyRetryAttempt(int attemptNumber) => TimeSpan.FromSeconds(Math.Pow(2, attemptNumber));
+        }
+        #endregion
     }
 }
---
+++ HackerNews/HackerNews/Services/IHackerNewsAPI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Refit;
+
+namespace HackerNews
+{
+    public interface IHackerNewsAPI
+    {
+        [Get("/topstories.json?print=pretty")]
+        Task<List<string>> GetTopStoryIDs();
+
+        [Get("/item/{storyId}.json?print=pretty")]
+        Task<StoryModel> GetStory(string storyId);
+    }
+}
