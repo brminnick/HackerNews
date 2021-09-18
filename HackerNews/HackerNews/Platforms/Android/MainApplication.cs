ef24c00669489fe6d28422c31353c48c8019ed9d
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-09-18 16:32:23 -0700

    Update for .NET 6.0 RC1 (#13)
    * Update Startup.cs
    
    * Update Startup Logic

--- HackerNews/HackerNews/Platforms/Android/MainApplication.cs
+++ HackerNews/HackerNews/Platforms/Android/MainApplication.cs
@@ -5,12 +5,13 @@ using Microsoft.Maui;
 
 namespace HackerNews
 {
-	[Application]
-	public class MainApplication : MauiApplication<Startup>
-	{
-		public MainApplication(IntPtr handle, JniHandleOwnership ownership)
-			: base(handle, ownership)
-		{
-		}
-	}
+    [Application]
+    public class MainApplication : MauiApplication
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership ownership) : base(handle, ownership)
+        {
+        }
+
+        protected override MauiApp CreateMauiApp() => Startup.Create();
+    }
 }
--- HackerNews/HackerNews/Startup.cs
+++ HackerNews/HackerNews/Startup.cs
@@ -1,36 +1,30 @@
-﻿using System;
-using Azure;
-using Azure.AI.TextAnalytics;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Maui;
 using Microsoft.Maui.Controls.Hosting;
 using Microsoft.Maui.Controls.Xaml;
-using Microsoft.Maui.Hosting;
 using Refit;
 
-[assembly: XamlCompilationAttribute(XamlCompilationOptions.Compile)]
-
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace HackerNews
 {
-    public class Startup : IStartup
-	{
-		public void Configure(IAppHostBuilder appBuilder)
-		{
-			appBuilder.UseMauiApp<App>()
-				.ConfigureServices(services =>
-				{
-					// Services
-					services.AddSingleton(new TextAnalyticsClient(new Uri(TextAnalysisConstants.BaseUrl), new AzureKeyCredential(TextAnalysisConstants.SentimentKey)));
-					services.AddSingleton(RestService.For<IHackerNewsAPI>("https://hacker-news.firebaseio.com/v0"));
-					services.AddSingleton<TextAnalysisService>();
-					services.AddSingleton<HackerNewsAPIService>();
+    public class Startup
+    {
+        public static MauiApp Create()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder.UseMauiApp<App>();
 
-					// View Models
-					services.AddTransient<NewsViewModel>();
+            // Services
+            builder.Services.AddSingleton(RestService.For<IHackerNewsAPI>("https://hacker-news.firebaseio.com/v0"));
+            builder.Services.AddSingleton<HackerNewsAPIService>();
 
-					// Pages
-					services.AddTransient<NewsPage>();
-				});
-		}
-	}
+            // View Models
+            builder.Services.AddTransient<NewsViewModel>();
+
+            // Pages
+            builder.Services.AddTransient<NewsPage>();
+
+            return builder.Build();
+        }
+    }
 }
