bbacfd565bbefbe9da04041826985f6ae0103d02
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2024-09-21 12:36:57 -0700

    Implement MobileHttpRetryStrategyOptions

--- src/HackerNews/MauiProgram.cs
+++ src/HackerNews/MauiProgram.cs
@@ -2,6 +2,7 @@
 using Azure.AI.TextAnalytics;
 using CommunityToolkit.Maui;
 using CommunityToolkit.Maui.Markup;
+using Microsoft.Extensions.Http.Resilience;
 using Polly;
 using Refit;
 
@@ -13,9 +14,9 @@ public class MauiProgram
 	public static MauiApp CreateMauiApp()
 	{
 		var builder = MauiApp.CreateBuilder()
-								.UseMauiApp<App>()
-								.UseMauiCommunityToolkit()
-								.UseMauiCommunityToolkitMarkup();
+			.UseMauiApp<App>()
+			.UseMauiCommunityToolkit()
+			.UseMauiCommunityToolkitMarkup();
 
 		builder.ConfigureMauiHandlers(handlers =>
 		{
@@ -35,15 +36,23 @@ public class MauiProgram
 		builder.Services.AddSingleton(new TextAnalyticsClient(new Uri(TextAnalysisConstants.BaseUrl), new AzureKeyCredential(TextAnalysisConstants.SentimentKey)));
 
 		builder.Services.AddRefitClient<IHackerNewsAPI>()
-							.ConfigureHttpClient(client => client.BaseAddress = new Uri("https://hacker-news.firebaseio.com/v0"))
-							.AddTransientHttpErrorPolicy(builder => builder.WaitAndRetryAsync(3, ExponentialBackoff));
+			.ConfigureHttpClient(static client => client.BaseAddress = new Uri("https://hacker-news.firebaseio.com/v0"))
+			.AddStandardResilienceHandler(static options => options.Retry = new MobileHttpRetryStrategyOptions());
 
 		// Pages + View Models
 		builder.Services.AddTransientWithShellRoute<NewsPage, NewsViewModel>($"//{nameof(NewsPage)}");
 
-
 		return builder.Build();
+	}
 
-		static TimeSpan ExponentialBackoff(int attemptNumber) => TimeSpan.FromSeconds(Math.Pow(2, attemptNumber));
+	sealed class MobileHttpRetryStrategyOptions : HttpRetryStrategyOptions
+	{
+		public MobileHttpRetryStrategyOptions()
+		{
+			BackoffType = DelayBackoffType.Exponential;
+			MaxRetryAttempts = 3;
+			UseJitter = true;
+			Delay = TimeSpan.FromSeconds(2);
+		}
 	}
 }
